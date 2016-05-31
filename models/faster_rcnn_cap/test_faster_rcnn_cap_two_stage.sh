fe5b80f7aab1b683f69b9c05963dec1cf6612d14
bogger <yljatthu@gmail.com>
2016-05-31 07:51:36 -0700

    evaluation code for model using lstm output for bbox regression

--- models/faster_rcnn_cap/test_faster_rcnn_cap_two_stage.sh
+++ models/faster_rcnn_cap/test_faster_rcnn_cap_two_stage.sh
@@ -1,10 +1,10 @@
  
-GPU_ID=1
-NET_FINAL=output/faster_rcnn_end2end/vg_train/faster_rcnn_cap_rpn_no_reg_iter_200000.caffemodel
+GPU_ID=0
+NET_FINAL=output/faster_rcnn_end2end/vg_train/faster_rcnn_cap_two_stage2_iter_200000.caffemodel
 TEST_IMDB="vg_test"
 PT_DIR="faster_rcnn_cap"
 time ./lib/tools/test_net_cap_im_first.py --gpu ${GPU_ID} \
-  --def_feature models/${PT_DIR}/vgg_region_feature_rpn_no_reg.prototxt \
+  --def_feature models/${PT_DIR}/vgg_region_feature_reg2.prototxt \
   --def_recurrent models/${PT_DIR}/test_cap_pred.prototxt \
   --def_embed models/${PT_DIR}/test_word_embedding.prototxt \
   --net ${NET_FINAL} \
