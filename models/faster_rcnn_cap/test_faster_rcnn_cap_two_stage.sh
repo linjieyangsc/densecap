9817659cd1e18a3dfeb7d3a7354dfb76af3e41d4
bogger <yljatthu@gmail.com>
2016-06-01 02:02:16 -0700

    add finetuning script

--- models/faster_rcnn_cap/test_faster_rcnn_cap_two_stage.sh
+++ models/faster_rcnn_cap/test_faster_rcnn_cap_two_stage.sh
@@ -1,11 +1,11 @@
  
 GPU_ID=0
-NET_FINAL=output/faster_rcnn_end2end/vg_train/faster_rcnn_cap_two_stage2_iter_200000.caffemodel
+NET_FINAL=output/faster_rcnn_end2end/vg_train/faster_rcnn_cap_two_stage_iter_200000.caffemodel
 TEST_IMDB="vg_test"
 PT_DIR="faster_rcnn_cap"
 time ./lib/tools/test_net_cap_im_first.py --gpu ${GPU_ID} \
-  --def_feature models/${PT_DIR}/vgg_region_feature_reg2.prototxt \
-  --def_recurrent models/${PT_DIR}/test_cap_pred.prototxt \
+  --def_feature models/${PT_DIR}/vgg_region_feature_reg.prototxt \
+  --def_recurrent models/${PT_DIR}/test_cap_pred1.prototxt \
   --def_embed models/${PT_DIR}/test_word_embedding.prototxt \
   --net ${NET_FINAL} \
   --imdb ${TEST_IMDB} \
